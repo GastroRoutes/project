8140fb6baff808912dc434961d1b29c5c5a2016e
aterron84 <aterron84@gmail.com>
2018-12-14 18:08:29 +0100

    Mapbox with two markers created

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,31 +1,50 @@
+import ReactMapboxGl, { Layer, Feature, Popup } from "react-mapbox-gl";
+import React, { Component } from 'react'
 
-import ReactMapGL from 'react-map-gl';
-import React, { Component } from 'react';
 
-class Map extends Component {
-constructor(){
-super()
+const Map = ReactMapboxGl({
+  accessToken:
+"pk.eyJ1IjoiZGRpZXpyIiwiYSI6ImNqb3ZuMGZ3cjFqa2YzcWxrYjBtNjJzaG4ifQ.cCFZkl39Hov3D-Ujeq74Cg"
+});
+const Zoom = [10];
+const mapStyle = { height: "100vh", width: "100vw", display: "flex" };
+const styles = { dark: "mapbox://styles/mapbox/light-v9" };
+const center = [-3.70379, 40.416775];
 
-}
 
-  state = {
-    viewport: {
-      width: 800,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
-    }
-  };
+export default class MapBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  render() {
-    const TOKEN = ""
-    return (
-      <ReactMapGL mapboxApiAccessToken={TOKEN}
-        {...this.state.viewport}
-        onViewportChange={(viewport)  => this.setState({viewport})}
-      />
-    );
+    };
   }
-}
-export default Map
+
+  //Component didmount: establecer la peticion get de axios a mapbox api documentation
+ 
+  render() {
+      return (
+        <Map
+          style={styles.dark}
+          zoom={Zoom}
+          containerStyle={mapStyle}
+          center={center}
+        >
+            <Layer
+              type="symbol"
+              id="marker"
+              layout={{ "icon-image": "marker-15","icon-size":1.25 }}>
+              <Feature coordinates={[-3.70379, 40.416775]}/>
+            </Layer>
+            <Layer
+              type="symbol"
+              id="marker2"
+              layout={{ "icon-image": "marker-15","icon-size":1.25 }}>
+              <Feature coordinates={[-3.698237699999936, 40.3932277]}/>
+            </Layer>
+        </Map>
+      );
+    }
+  }
+
+  
