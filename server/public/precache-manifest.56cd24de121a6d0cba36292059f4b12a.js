f3be99879305d523feb25e41e41f643b38bdbf8c
aterron84 <aterron84@gmail.com>
2019-01-16 21:58:14 +0100

    Heroku deploy

---
+++ server/public/precache-manifest.56cd24de121a6d0cba36292059f4b12a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64126d3c041fb0e116af",
+    "url": "/static/js/main.64126d3c.chunk.js"
+  },
+  {
+    "revision": "6e059cac6c1f8303cc3a",
+    "url": "/static/js/1.6e059cac.chunk.js"
+  },
+  {
+    "revision": "64126d3c041fb0e116af",
+    "url": "/static/css/main.faa76b33.chunk.css"
+  },
+  {
+    "revision": "6ac5c9be225de1fde5a72886adaa2a5a",
+    "url": "/index.html"
+  }
+];
